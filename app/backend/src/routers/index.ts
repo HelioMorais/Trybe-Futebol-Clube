bfa45fed07f7577b7831eb1ea8ac7c5425a13317
Helio Morais <heliogov20@gmail.com>
2023-09-22 15:15:35 -0300

    Requisito 8

--- app/backend/src/routers/index.ts
+++ app/backend/src/routers/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import teamsRouter from './teams.routers';
+import loginRouter from './login.routers';
 
 const router = Router();
 
 router.use('/teams', teamsRouter);
+router.use('/login', loginRouter);
 
 export default router;
